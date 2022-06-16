0a2104f988de1d41dab182e77def345788ff0548
Fabian Bull <spam+github@fbull.de>
2022-06-16 19:56:23 +0200

    Improve provision script

--- keycloak/src/provision.ts
+++ keycloak/src/provision.ts
@@ -1,28 +1,15 @@
 import KcAdminClient from "@keycloak/keycloak-admin-client";
+import { STUDENT_GROUP, TEACHER_GROUP, User, student, studentR, teacherR } from "./domain"
 import Chance from "chance";
 
-// To configure the client, pass an object to override any of these  options:
-// {
-//   baseUrl: 'http://127.0.0.1:8080/auth',
-//   realmName: 'master',
-//   requestConfig: {
-//     /* Axios request config options https://github.com/axios/axios#request-config */
-//   },
-// }
+const chance = new Chance();
 
 const RealmName = "openschool";
-
-const TODAY = new Date(Date.now())
-
-const SCHOOL_ENTRY_AGE = 6
-
-const SCHOOL_EXIT_AGE = 19;
 
 (async () => {
   const kcAdminClient = new KcAdminClient({
     baseUrl: 'http://127.0.0.1:8088/auth',
   });
-
 
   // Authorize with username / password
   await kcAdminClient.auth({
@@ -32,133 +19,72 @@ const SCHOOL_EXIT_AGE = 19;
     clientId: "admin-cli",
   });
 
-  await kcAdminClient.realms.create({
-    enabled: true,
-    realm: RealmName
-  })
+  try {
+    await kcAdminClient.realms.create({
+      enabled: true,
+      realm: RealmName
+    })
+    console.log("Created the realm")
+  } catch (error) {
+    console.error("Failed to create the realm")
+  }
 
   // Override client configuration for all further requests:
   kcAdminClient.setConfig({
     realmName: RealmName
   });
 
-  await kcAdminClient.clients.create({
-    enabled: true,
-    clientId: `${RealmName}-frontend`,
-    rootUrl: "http://localhost:8000",
-    redirectUris: ["http://localhost:8000/*"],
-    adminUrl: "http://localhost:8000",
-    webOrigins: ["http://localhost:8000"],
-    protocol: "openid-connect",
-  })
+  try {
+    await kcAdminClient.clients.create({
+      enabled: true,
+      clientId: `${RealmName}-frontend`,
+      rootUrl: "http://localhost:8000",
+      redirectUris: ["http://localhost:8000/*"],
+      adminUrl: "http://localhost:8000",
+      webOrigins: ["http://localhost:8000"],
+      protocol: "openid-connect",
+    })
+    console.log("Created the client")
+  } catch (error) {
+    console.error("Failed to create the client")
+  }
 
-  const chance = new Chance();
+  try {
+    const students = await kcAdminClient.groups.create({ name: STUDENT_GROUP });
+    console.log(`Created group students: ${students.id}`)
 
-  const students = await kcAdminClient.groups.create({
-    name: "students"
-  });
-  console.log(`Created group students: ${students.id}`)
+    const teachers = await kcAdminClient.groups.create({ name: TEACHER_GROUP });
+    console.log(`Created group teachers: ${teachers.id}`)
+  } catch (error) {
+    console.error("Failed to create the groups")
+  }
 
-  const teachers = await kcAdminClient.groups.create({
-    name: "teachers"
-  });
-  console.log(`Created group teachers: ${teachers.id}`)
+  async function createUser(user: User): Promise<{ id: string }> {
+    try {
+      const u = await kcAdminClient.users.create(user);
+      console.log(`Created user: ${u.id}`)
+      return u
+    } catch (error) {
+      console.warn(`Failed to create user: ${user.email}`)
+    }
+  }
 
   // Standard users
-  const gender: "male" | "female" = (chance.gender() == "male") ? "male" : "female";
-  await kcAdminClient.users.create({
-    username: "anna@anna.com",
-    email: "anna@anna.com",
-    firstName: "Anna",
-    lastName: "Baxx",
-    emailVerified: true,
-    enabled: true,
-    groups: ["students"],
-    credentials: [{
-      type: "password",
-      value: "anna"
-    }],
-    attributes: {
-      gender: gender,
-      birthday: chance.birthday({
-        year: chance.year({
-          max: TODAY.getFullYear() - SCHOOL_ENTRY_AGE,
-          min: TODAY.getFullYear() - SCHOOL_EXIT_AGE,
-        }),
-      }),
-      address: chance.address(),
-      zip: chance.zip(),
-      city: chance.city(),
-      country: chance.country({ full: true }),
-    }
-  });
+  const anna = student("anna", "anna", chance)
+  const joe = student("joe", "joe", chance)
+  await createUser(anna)
+  await createUser(joe)
 
   // Students
   const number_of_students = 100
-
   for (let i = 0; i < number_of_students; i++) {
-    const gender: "male" | "female" = (chance.gender() == "male") ? "male" : "female";
-    const email = chance.email()
-    const firstName = chance.first({ gender })
-
-    const user = await kcAdminClient.users.create({
-      username: email,
-      email: email,
-      firstName: firstName,
-      lastName: chance.last(),
-      emailVerified: true,
-      enabled: true,
-      groups: ["students"],
-      credentials: [{
-        type: "password",
-        value: firstName
-      }],
-      attributes: {
-        gender: gender,
-        birthday: chance.birthday({
-          year: chance.year({
-            max: TODAY.getFullYear() - SCHOOL_ENTRY_AGE,
-            min: TODAY.getFullYear() - SCHOOL_EXIT_AGE,
-          }),
-        }),
-        address: chance.address(),
-        zip: chance.zip(),
-        city: chance.city(),
-        country: chance.country({ full: true }),
-      }
-    });
-    console.log(`Created user: ${user.id}`)
+    createUser(studentR(chance))
   }
 
   // Teacher
   const number_of_teachers = 50
-
   for (let i = 0; i < number_of_teachers; i++) {
-    const gender: "male" | "female" = (chance.gender() == "male") ? "male" : "female";
-    const email = chance.email()
-    const firstName = chance.first({ gender })
-    const teacher = await kcAdminClient.users.create({
-      username: email,
-      email: email,
-      firstName: firstName,
-      lastName: chance.last(),
-      emailVerified: true,
-      enabled: true,
-      groups: ["teachers"],
-      credentials: [{
-        type: "password",
-        value: firstName
-      }],
-      attributes: {
-        gender: gender,
-        birthday: chance.birthday({ type: 'adult' }),
-        address: chance.address(),
-        zip: chance.zip(),
-        city: chance.city(),
-        country: chance.country({ full: true }),
-      }
-    })
-    console.log(`Created teacher: ${teacher.id}`)
+    createUser(teacherR(chance))
   }
 })();
 
